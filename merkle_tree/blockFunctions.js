da61e14711401c8bfa3cc88f3ab303bccde60fec
power-serge <sy120687@hotmail.com>
2019-11-03 02:38:35 -0500

    Adding a couple of files to begin building the merkle tree demo web app

---
+++ merkle_tree/blockFunctions.js
@@ -0,0 +1,36 @@
+function fetchLatestBlock(){
+	fetch(`https://blockchain.info/q/latesthash?cors=true`)
+		.then(r => r.text())
+		.then(txt => document.getElementById("demo").innerHTML = txt);
+}
+
+function fetchBlock(){
+//const block = '0000000000000a3290f20e75860d505ce0e948a1d1d846bec7e39015d242884b';
+var blockNumber = document.getElementById("block");
+const block = blockNumber.value;
+fetch(`https://blockchain.info/rawblock/${block}?cors=true`)
+    .then(r => r.json())
+    .then(d => [d.mrkl_root, d.tx.map(t => t.hash), d.height])
+	.then( ([root, txs, height]) => {
+		document.getElementById("demo1").innerHTML = height;
+		document.getElementById("demo2").innerHTML = root;
+		
+		var txt = "";
+		for (x of txs)
+		{
+			txt += x + "<br>";
+		}
+		document.getElementById("demo3").innerHTML = txt;
+		
+	});
+}
+
+function myFunction() {
+  var x = document.getElementById("blockForm");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+    text += x.elements[i].value + "<br>";
+  }
+  document.getElementById("blockNumber").innerHTML = text;
+}
