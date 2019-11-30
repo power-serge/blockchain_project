9acb0edbed97391733e30683811b1e3237e53cba
power-serge <sy120687@hotmail.com>
2019-11-30 17:30:03 -0500

    Updating functions in JS and some text

--- merkle_tree/blockFunctions.js
+++ merkle_tree/blockFunctions.js
@@ -1,71 +1,81 @@
-function fetchLatestBlock() {
-    fetch(`https://blockchain.info/q/latesthash?cors=true`)
-    .then(r => r.text())
-    .then(txt => document.getElementById("demo").innerHTML = txt);
-}
+/* Global Variables */
+/* Returns the hash of the latest block */
+const LATEST_HASH = () =>
+	fetch(`https://blockchain.info/q/latesthash?cors=true`)
+		.then(r => r.text());
 
-function fetchBlock() {
-    //const block = '0000000000000a3290f20e75860d505ce0e948a1d1d846bec7e39015d242884b';
-    var blockNumber = document.getElementById("block");
-    const block = blockNumber.value;
+/* Returns the root, the array of transaction hashes, and the height */
+const BLOCK_INFO = block =>
     fetch(`https://blockchain.info/rawblock/${block}?cors=true`)
     .then(r => r.json())
-    .then(d => [d.mrkl_root, d.tx.map(t => t.hash), d.height])
-    .then(([root, txs, height]) => {
+    .then(d => [d.mrkl_root, d.tx.map(t => t.hash), d.height, d.hash])
 
-        document.getElementById("demo1").innerHTML = height;
-        document.getElementById("demo2").innerHTML = root;
-
-        var txsText = document.getElementById("block_size");
-        var text = txsText.innerHTML;
-
-        txsText.innerHTML = text.substring(0, text.indexOf(":") + 1);
-        txsText.innerHTML += " " + txs.length;
-
-        var txt = "";
-        for (var x of txs) {
-            txt += x + "<br>";
-        }
-        document.getElementById("demo3").innerHTML = txt;
-
-    });
+/* Displays the hash of the lates added block to the Bitcoin chain */
+function displayLatestBlock() {
+	LATEST_HASH().then(hash => document.getElementById("demo").innerHTML = hash);
 }
 
-function fetchBlockByHeight() {
-    
+function fetchBlockByHash() {
+	const block_hash = document.getElementById("block").value;
+	
+	if (block_hash.length != 64)
+	{
+		alert("Please Enter a Valid Block hash")
+	}
+	else
+	{
+		parseBlockInfo(block_hash);
+	}
+}
+
+function fetchBlockByHeight() 
+{
 	const height = document.getElementById("height").value;
-
-    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = 'https://blockchain.info/block-height/' + height + '?format=json'
-        fetch(proxyUrl + targetUrl)
-        .then(blob => blob.json())
-        .then(data => data.blocks[0].hash)
-        .then(block => {
-            fetch(`https://blockchain.info/rawblock/${block}?cors=true`)
-            .then(r => r.json())
-            .then(d => [d.mrkl_root, d.tx.map(t => t.hash), d.height])
-            .then(([root, txs, height]) => {
-
-                document.getElementById("demo1").innerHTML = height;
-                document.getElementById("demo2").innerHTML = root;
-
-                var txsText = document.getElementById("block_size");
-                var text = txsText.innerHTML;
-
-                txsText.innerHTML = text.substring(0, text.indexOf(":") + 1);
-                txsText.innerHTML += " " + txs.length;
-
-                var txt = "";
-                for (var x of txs) {
-                    txt += x + "<br>";
-                }
-                document.getElementById("demo3").innerHTML = txt;
-
-			})
-		});
+	if (height == "" || isNaN(height))
+	{
+		alert("Please Enter a Valid Number");
+	}
+	else
+	{
+		var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+		var targetUrl = 'https://blockchain.info/block-height/' + height + '?format=json';
+			fetch(proxyUrl + targetUrl)
+			.then(blob => blob.json())
+			.then(data => data.blocks[0].hash)
+			.then(block_hash => {
+				parseBlockInfo(block_hash);
+			});
+	}
 }
-            
-        
+
+function parseBlockInfo(block_hash) 
+{
+	BLOCK_INFO(block_hash).then(([root, txs, height, block_id]) => {
+
+		if (root == null || txs == null || height == null )
+		{
+			alert("An error occurred. \nCould not fetch the data")
+		}
+		else
+		{
+			displayData(root, txs, height, block_id)
+		}
+	});
+}
+
+function displayData(root, txs, height, block_id) 
+{
+	document.getElementById("block_id").innerHTML = block_id;
+	document.getElementById("block_num").innerHTML = height;
+	document.getElementById("block_mrkl").innerHTML = root;
+	document.getElementById("block_size").innerHTML = txs.length;
+
+	var txt = "";
+	for (var x of txs) {
+		txt += x + "<br>";
+	}
+	document.getElementById("block_txs").innerHTML = txt;
+}
 
 function fetchOnEnter(button) {
     console.log(button);
@@ -73,14 +83,4 @@ function fetchOnEnter(button) {
         event.preventDefault();
         fetchBlock();
     }
-}
-
-function myFunction() {
-    var x = document.getElementById("blockForm");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length; i++) {
-        text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("blockNumber").innerHTML = text;
 }
