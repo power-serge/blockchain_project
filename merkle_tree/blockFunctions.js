957da585fa64a745e01bff2c96619ee5e898fe4c
power-serge <sy120687@hotmail.com>
2019-11-30 23:33:50 -0500

    Adding several files
    Added CSS for merkle page
    Updated more descriptions
    Added block functions to merkle page
    Added functions to js functions

--- merkle_tree/blockFunctions.js
+++ merkle_tree/blockFunctions.js
@@ -1,15 +1,37 @@
-/* Global Variables */
+/* Constants */
 /* Returns the hash of the latest block */
 const LATEST_HASH = () =>
 	fetch(`https://blockchain.info/q/latesthash?cors=true`)
-		.then(r => r.text());
+	.then(r => r.text());
 
-/* Returns the root, the array of transaction hashes, and the height */
+/* Returns the root, the array of transaction hashes, the height, and the hash given a block hash */
 const BLOCK_INFO = block =>
-    fetch(`https://blockchain.info/rawblock/${block}?cors=true`)
-    .then(r => r.json())
-    .then(d => [d.mrkl_root, d.tx.map(t => t.hash), d.height, d.hash])
+	fetch(`https://blockchain.info/rawblock/${block}?cors=true`)
+	.then(r => r.json())
+	.then(d => [d.mrkl_root, d.tx.map(t => t.hash), d.height, d.hash])
 
+const toBytes = hex =>
+	hex.match(/../g).reduce((acc, hex) => [...acc, parseInt(hex, 16)], []);
+
+const toHex = bytes =>
+	bytes.reduce((acc, bytes) => acc + bytes.toString(16).padStart(2, '0'), '');
+
+const toPairs = arr =>
+	Array.from(Array(Math.ceil(arr.length / 2)), (_, i) => arr.slice(i * 2, i * 2 + 2));
+
+const hashPair = (a, b = a) => {
+	const bytes = toBytes(`${b}${a}`).reverse();
+	const hashed = sha256.array(sha256.array(bytes));
+	return toHex(hashed.reverse());
+};
+
+const hashOne = data => {
+	const bytes = toBytes(`${data}`).reverse();
+	const hash = sha256.array(bytes);
+	return toHex(hash.reverse());
+}
+
+/* Functions */
 /* Displays the hash of the lates added block to the Bitcoin chain */
 function displayLatestBlock() {
 	LATEST_HASH().then(hash => document.getElementById("demo").innerHTML = hash);
@@ -17,54 +39,41 @@ function displayLatestBlock() {
 
 function fetchBlockByHash() {
 	const block_hash = document.getElementById("block").value;
-	
-	if (block_hash.length != 64)
-	{
+
+	if (block_hash.length != 64) {
 		alert("Please Enter a Valid Block hash")
-	}
-	else
-	{
+	} else {
 		parseBlockInfo(block_hash);
 	}
 }
 
-function fetchBlockByHeight() 
-{
+function fetchBlockByHeight() {
 	const height = document.getElementById("height").value;
-	if (height == "" || isNaN(height))
-	{
+	if (height == "" || isNaN(height)) {
 		alert("Please Enter a Valid Number");
-	}
-	else
-	{
+	} else {
 		var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 		var targetUrl = 'https://blockchain.info/block-height/' + height + '?format=json';
-			fetch(proxyUrl + targetUrl)
-			.then(blob => blob.json())
-			.then(data => data.blocks[0].hash)
-			.then(block_hash => {
-				parseBlockInfo(block_hash);
-			});
+		fetch(proxyUrl + targetUrl)
+		.then(blob => blob.json())
+		.then(data => data.blocks[0].hash)
+		.then(block_hash => {
+			parseBlockInfo(block_hash);
+		});
 	}
 }
 
-function parseBlockInfo(block_hash) 
-{
+function parseBlockInfo(block_hash) {
 	BLOCK_INFO(block_hash).then(([root, txs, height, block_id]) => {
-
-		if (root == null || txs == null || height == null )
-		{
+		if (root == null || txs == null || height == null) {
 			alert("An error occurred. \nCould not fetch the data")
-		}
-		else
-		{
+		} else {
 			displayData(root, txs, height, block_id)
 		}
 	});
 }
 
-function displayData(root, txs, height, block_id) 
-{
+function displayData(root, txs, height, block_id) {
 	document.getElementById("block_id").innerHTML = block_id;
 	document.getElementById("block_num").innerHTML = height;
 	document.getElementById("block_mrkl").innerHTML = root;
@@ -77,10 +86,17 @@ function displayData(root, txs, height, block_id)
 	document.getElementById("block_txs").innerHTML = txt;
 }
 
-function fetchOnEnter(button) {
-    console.log(button);
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        fetchBlock();
-    }
+function fetchOnEnter(input_id) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+
+		if (input_id == "block") {
+			fetchBlockByHash();
+			return;
+		}
+
+		if (input_id == "height") {
+			fetchBlockByHeight();
+		}
+	}
 }
