865d66eb1a83bd7ba7011587f692068c0b56d4af
power-serge <sy120687@hotmail.com>
2019-12-08 16:40:58 -0500

    Added more info to the merkle page along with styles and a hashIt2 function to merkle.js

--- js/merkle.js
+++ js/merkle.js
@@ -12,3 +12,18 @@ function hashIt (valueToHash) {
 	}
 }
 
+function hashIt2 (valueToHash) {
+	const HASH_TEXT = 
+		"<br><br>You just performed a SHA-256 double hash!."
+	let hash_p = document.getElementById('hash2');
+	
+	hash_p.innerHTML = hashOne(valueToHash);
+	text_p = document.getElementById('hide_it');
+	
+	if (!hash_one.innerHTML.includes("Please") && valueToHash.length == 64)
+	{
+		hash_p.innerHTML += HASH_TEXT;
+		text_p.style.display = "none";
+	}	
+}
+
